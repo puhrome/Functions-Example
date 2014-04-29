c3a4ea2e9cd5ba12858cb98a90357d03d500bdc3
puhrome <phirom.yim@fullsail.edu>
2014-04-29 17:58:23 -0400

    Functions Examples
    Examples of what a Function is in WebStorm editor

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Functions Example Created by Phirom Yim on 4/29/14.
+ */
+//Function example:
+//myTemp =  convertToCentigrade(212);
+//centigrade - Function
+//212 - parameter
+//return - stored in myTemp Variable
+//Beginning JavaScript Example:
+
+function writeUserWelcome(userName, userAge){
+    console.log("Welcome to my website + userName + ");
+        console.log("Hope you enjoy it!");
+}
+writeUserWelcome("Paul", 31);
